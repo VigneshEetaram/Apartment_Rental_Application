33c79663da9aa68bbaad0d479931319e8f46256d
jg52can <jobingrg52@gmail.com>
2021-02-15 16:48:51 -0500

    Apartmentdetails

--- app/src/main/java/com/example/rentalapp/ApartmentDetails.java
+++ app/src/main/java/com/example/rentalapp/ApartmentDetails.java
@@ -12,12 +12,29 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.squareup.picasso.Picasso;
 
 public class ApartmentDetails extends AppCompatActivity {
-
+    private TextView Price,Description,Title,Place;
+    ImageView Image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_apartment_details);
 
+        Image = (ImageView) findViewById(R.id.img_apt_details);
+        Price= (TextView) findViewById(R.id.txt_view_price);
+        Description= (TextView) findViewById(R.id.txt_view_description);
+        Title = (TextView) findViewById(R.id.txt_view_title);
+        Place = (TextView) findViewById(R.id.txt_view_place);
+
+        Intent intent = getIntent();
+        Picasso.get().load(intent.getStringExtra("Image")).into(Image);
+        String price = intent.getExtras().getString("Price");
+        String title = intent.getExtras().getString("Title");
+        String place = intent.getExtras().getString("Place");
+        String description = intent.getExtras().getString("Description");
+        Price.setText(price);
+        Title.setText(title);
+        Place.setText(place);
+        Description.setText(description);
     }
 }
--- app/src/main/java/com/example/rentalapp/ApartmentEditDetails.java
+++ app/src/main/java/com/example/rentalapp/ApartmentEditDetails.java
@@ -14,10 +14,82 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.squareup.picasso.Picasso;
 
 public class ApartmentEditDetails extends AppCompatActivity {
-
+    private TextView Price,Description,Title,Place;
+    ImageView Image, edit;
+    Button btn_save;
+    FirebaseAuth fAuth;
+    FirebaseFirestore db;
+    String userID;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_apartment_edit_details);
+
+        Image = (ImageView) findViewById(R.id.img_apt_edit_details);
+        edit = (ImageView) findViewById(R.id.img_edit);
+        Price= (TextView) findViewById(R.id.txt_view_edit_price);
+        Description= (TextView) findViewById(R.id.txt_view_edit_description);
+        Title = (TextView) findViewById(R.id.txt_view_edit_title);
+        Place = (TextView) findViewById(R.id.txt_view_edit_place);
+        btn_save = (Button) findViewById(R.id.btn_apartment_edit_details);
+        fAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
+        userID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+        Intent intent = getIntent();
+        Picasso.get().load(intent.getStringExtra("Image")).into(Image);
+        String id = intent.getExtras().getString("Id");
+        String price = intent.getExtras().getString("Price");
+        String title = intent.getExtras().getString("Title");
+        String place = intent.getExtras().getString("Place");
+        String description = intent.getExtras().getString("Description");
+
+        btn_save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                db.collection("Apartments").document(id)
+                        .update(
+                                "title", Title.getText().toString(),
+                                "price", Title.getText().toString(),
+                                "place", Place.getText().toString(),
+                                "description", Description.getText().toString()
+                        );
+
+                db.collection("Myads").document(userID).collection("Selected").document(id)
+                        .update(
+                                "title", Title.getText().toString(),
+                                "price", Title.getText().toString(),
+                                "place", Place.getText().toString(),
+                                "description", Description.getText().toString()
+                        );
+
+                Price.setEnabled(false);
+                Title.setEnabled(false);
+                Place.setEnabled(false);
+                Description.setEnabled(false);
+                btn_save.setVisibility(View.INVISIBLE);
+            }
+
+
+        });
+
+
+        Price.setText(price);
+        Title.setText(title);
+        Place.setText(place);
+        Description.setText(description);
+
+        edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Price.setEnabled(true);
+                Title.setEnabled(true);
+                Place.setEnabled(true);
+                Description.setEnabled(true);
+                btn_save.setVisibility(View.VISIBLE);
+            }
+        });
+
     }
 }
