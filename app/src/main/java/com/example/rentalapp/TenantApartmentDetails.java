72fcc9242a6c5de04847cee54cd4230c6354e9f7
jg52can <jobingrg52@gmail.com>
2021-02-15 18:48:38 -0500

    Apartmentdetails

--- app/src/main/java/com/example/rentalapp/TenantApartmentDetails.java
+++ app/src/main/java/com/example/rentalapp/TenantApartmentDetails.java
@@ -13,14 +13,41 @@ import com.google.gson.internal.$Gson$Preconditions;
 import com.squareup.picasso.Picasso;
 
 public class TenantApartmentDetails extends AppCompatActivity {
-
+    private TextView Price,Description,Title,Place;
+    ImageView Image;
+    Button btn_showMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tenant_apartment_details);
 
+        Image = (ImageView) findViewById(R.id.img_apt_details_tenant);
+        Price= (TextView) findViewById(R.id.txt_view_price_tenant);
+        Description= (TextView) findViewById(R.id.txt_view_description_tenant);
+        Title = (TextView) findViewById(R.id.txt_view_title_tenant);
+        Place = (TextView) findViewById(R.id.txt_view_place_tenant);
+        btn_showMap = (Button) findViewById(R.id.btn_tenant_apartment_details);
 
+        Intent intent = getIntent();
+        Picasso.get().load(intent.getStringExtra("Image")).into(Image);
+        String price = intent.getExtras().getString("Price");
+        String title = intent.getExtras().getString("Title");
+        String place = intent.getExtras().getString("Place");
+        String description = intent.getExtras().getString("Description");
+        Price.setText(price);
+        Title.setText(title);
+        Place.setText(place);
+        Description.setText(description);
+
+        btn_showMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(TenantApartmentDetails.this,MapsActivity.class);
+                intent.putExtra("Address",title+" "+place);
+                startActivity(intent);
+            }
+        });
 
     }
 }
