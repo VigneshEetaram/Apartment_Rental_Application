b632ad9acd80a8c51399d30ecac4f8d09e433b03
jg52can <jobingrg52@gmail.com>
2021-02-14 21:48:37 -0500

    updated - account, fourth and mydas fragments

--- app/src/main/java/com/example/rentalapp/AccountFragment.java
+++ app/src/main/java/com/example/rentalapp/AccountFragment.java
@@ -19,58 +19,14 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class AccountFragment extends Fragment {
-    EditText firstName, lastName, email, phoneNumber;
-    TextView changePassword, LogOut;
-    private FirebaseFirestore firebaseFirestore;
-    FirebaseAuth fAuth;
-    FirebaseFirestore db;
-    String userID;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v =inflater.inflate(R.layout.fragment_account, container, false);
-        firstName = v.findViewById(R.id.editTxt_Tenant_FN);
-        lastName = v.findViewById(R.id.editTxt_Tenant_LN);
-        email = v.findViewById(R.id.editTxt_Tenant_email);
-        phoneNumber = v.findViewById(R.id.editTxt_Tenant_phone);
-        changePassword = v.findViewById(R.id.txtView_Tenant_Change_Password);
 
-        fAuth = FirebaseAuth.getInstance();
-        db = FirebaseFirestore.getInstance();
-        userID = FirebaseAuth.getInstance().getCurrentUser().getUid();
-
-        changePassword.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getContext(),UpdatePassword.class));
-            }
-        });
-
-        DocumentReference docRef = db.collection("Users").document(userID);
-        docRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-            @Override
-            public void onSuccess(DocumentSnapshot documentSnapshot) {
-                User user = documentSnapshot.toObject(User.class);
-
-                firstName.setText(user.getFullname());
-                email.setText(user.getEmails());
-                phoneNumber.setText(user.getPhonenumber());
-            }
-        });
-
-
-
-
-        LogOut = v.findViewById(R.id.txtView_Renter_Logout);
-        LogOut.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FirebaseAuth.getInstance().signOut();
-                startActivity(new Intent(getContext(),MainActivity.class));
-            }
-        });
         return  v;
     }
 }
