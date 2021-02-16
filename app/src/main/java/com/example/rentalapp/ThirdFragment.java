2b1fa50411379473c7d5b743c6717cb28848743f
jg52can <jobingrg52@gmail.com>
2021-02-15 19:36:10 -0500

    ThirdFragment

--- app/src/main/java/com/example/rentalapp/ThirdFragment.java
+++ app/src/main/java/com/example/rentalapp/ThirdFragment.java
@@ -35,18 +35,60 @@ import java.util.Map;
 
 
 public class ThirdFragment extends Fragment {
+    ViewPager viewPager;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_third, container, false);
 
-
+        final TabLayout tableLayout = v.findViewById(R.id.tab_layout);
+        viewPager = v.findViewById(R.id.view_pager);
+        FragmentTransaction ft = getActivity().getSupportFragmentManager().beginTransaction();
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getActivity().getSupportFragmentManager());
+        viewPagerAdapter.addFragment(new MyadsFragment(), "My ads");
+        viewPagerAdapter.addFragment(new AllAdsFragment(), "All ads");
+        viewPager.setAdapter(viewPagerAdapter);
+        tableLayout.setupWithViewPager(viewPager);
 
         return v;
 
     }
 
+    class ViewPagerAdapter extends FragmentStatePagerAdapter {
+
+        private ArrayList<Fragment> fragments;
+        private ArrayList<String> titles;
+
+        ViewPagerAdapter(FragmentManager fm){
+            super(fm);
+            this.fragments = new ArrayList<>();
+            this.titles = new ArrayList<>();
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return fragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragments.size();
+        }
+
+        public void addFragment(Fragment fragment, String title){
+            fragments.add(fragment);
+            titles.add(title);
+        }
+
+
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return titles.get(position);
+        }
+    }
 
 
 }
