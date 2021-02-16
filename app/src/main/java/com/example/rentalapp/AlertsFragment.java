af74a25e883ec509eb3b15736843a0fad9456c5e
jg52can <jobingrg52@gmail.com>
2021-02-15 22:36:09 -0500

    Alerts

--- app/src/main/java/com/example/rentalapp/AlertsFragment.java
+++ app/src/main/java/com/example/rentalapp/AlertsFragment.java
@@ -1,64 +1,101 @@
 package com.example.rentalapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AlertsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.squareup.picasso.Picasso;
+
 public class AlertsFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public AlertsFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AlertsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AlertsFragment newInstance(String param1, String param2) {
-        AlertsFragment fragment = new AlertsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
+    private FirebaseFirestore firebaseFirestore;
+    private RecyclerView recyclerView;
+    private FirestoreRecyclerAdapter adapter;
+    FirebaseAuth fAuth;
+    FirebaseFirestore db;
+    String userID;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_alerts, container, false);
+        View view = inflater.inflate(R.layout.fragment_alerts, container, false);
+        recyclerView = view.findViewById(R.id.recyclerView10);
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        fAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
+        userID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+        Query query = firebaseFirestore.collection("Announcement");
+        FirestoreRecyclerOptions<Alerts> options = new FirestoreRecyclerOptions.Builder<Alerts>()
+                .setQuery(query,Alerts.class)
+                .build();
+        adapter = new FirestoreRecyclerAdapter<Alerts, ItemViewHolder>(options) {
+            @NonNull
+            @Override
+            public ItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.alertsrecyclerview,parent,false);
+                return new ItemViewHolder(v);
+            }
+
+            @Override
+            protected void onBindViewHolder(@NonNull ItemViewHolder holder, int position, @NonNull Alerts model) {
+                holder.Date.setText(model.getDate());
+                holder.Title.setText(model.getTitle());
+                holder.Description.setText(model.getDescription());
+            }
+        };
+
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new GridLayoutManager(getContext(),1));
+        recyclerView.setAdapter(adapter);
+
+        return view;
+    }
+
+    static class ItemViewHolder extends RecyclerView.ViewHolder {
+
+        TextView Title;
+        TextView Date;
+        TextView Description;
+
+
+        public ItemViewHolder(@NonNull View itemView) {
+            super(itemView);
+            Title = itemView.findViewById(R.id.txt_alerts_title_recycler);
+            Date = itemView.findViewById(R.id.txt_alerts_date_recycler);
+            Description = itemView.findViewById(R.id.txt_alerts_description_recycler);
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        adapter.stopListening();
     }
 }
