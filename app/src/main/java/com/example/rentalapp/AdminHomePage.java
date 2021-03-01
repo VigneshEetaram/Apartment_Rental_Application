e0d2a6c53e2ad43499d3c550c9c9c40e3e8b4900
jg52can <jobingrg52@gmail.com>
2021-03-01 06:58:57 -0500

    modifications

--- app/src/main/java/com/example/rentalapp/AdminHomePage.java
+++ app/src/main/java/com/example/rentalapp/AdminHomePage.java
@@ -1,6 +1,7 @@
 package com.example.rentalapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
@@ -14,7 +15,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class AdminHomePage extends AppCompatActivity {
-
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,5 +26,16 @@ public class AdminHomePage extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this,  R.id.fragment3);
         NavigationUI.setupWithNavController(bottomNavigationView, navController);
 
+        toolbar = findViewById(R.id.admin_home_page_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(AdminHomePage.this,AdminHomePage.class));
+                finish();
+            }
+        });
     }
 }
